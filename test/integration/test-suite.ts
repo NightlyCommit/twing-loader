823f77d166b4422a3a7dacbb53380614763ee34b
Eric MORAND <eric.morand@gmail.com>
2019-08-14 10:27:01 +0200

    Fix issue #4

--- test/integration/test-suite.ts
+++ test/integration/test-suite.ts
@@ -20,7 +20,10 @@ export class TestSuite {
             }
 
             return Promise.all(promises)
-                .finally(() => {
+                .then(() => {
+                    test.end();
+                })
+                .catch(() => {
                     test.end();
                 })
         });
